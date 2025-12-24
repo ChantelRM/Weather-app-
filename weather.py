6750bb51f0d26644ca0894b1c99aad26884dfecb
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2025-12-24 22:42:59 +0200

    api call, errors fixed

--- weather.py
+++ weather.py
@@ -3,14 +3,14 @@ import math
 
 
 class Weather:
-    def __init__(self,city,api_key,data = None):
+    def __init__(self,city,api_key):
         self.city = city
         self.api_key = api_key
-        self.data = data
+        self.data = 'None'
     #  key = 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
     # key 5c940fbc1f7fa426e6c24c05fd945af8
 
-    def fetch_data(self,city,api):
+    def fetch_data(self):
         '''
         Docstring for fetch_data
         This function gets data about the city's weather from the OpenWeather api
@@ -18,24 +18,30 @@ class Weather:
         
         :param self: Description
         '''
+
         # https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units=metric'
         # url= f'https://api.openweathermap.org/data/2.5/weather?lat={self.city}&lon={self.city}&appid={self.api_key}'
         
         # send a get request to url
-        response = requests.get(url).json() #converting to json allows us to access individual attributes
-        if response.status_code == 200:
-            return response
-        else :
-            print(f'Error {response.status_code} - {response.text}')
-            
+        try:
+            response = requests.get(url) #converting to json allows us to access individual attributes
+            self.data =response.json()
+            for key,value in self.data.items():
+                print(key,value)
+            # print(self.data)
+            # return self.data
+        except requests.exceptions.RequestException as e:
+            print(f'Error fetching data {e}')
+
     def get_temp(self):
         '''
         Docstring for get_temp
         This function fetches the temperature of he entered city from the api
         :param self: Description
         '''
-        pass
+        return self.data['main']
 
     def get_condition(self):
         '''
@@ -55,6 +61,8 @@ class Weather:
         '''
         pass
 
-weather = Weather()
-city = input('City,Country')
+
+city = input('City,Country:     ')
+weather = Weather(city,api_key = '5c940fbc1f7fa426e6c24c05fd945af8')
 weather.fetch_data()
+print(weather.get_temp())
