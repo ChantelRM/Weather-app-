4644eeb709724b6b682d17bca86c0694adc1843a
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2025-12-26 16:18:51 +0200

    reove .env from track

--- weather.py
+++ weather.py
@@ -1,5 +1,9 @@
 import requests
-import math
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key= os.getenv('api_key')
 
 # TODO:
     # - Add the get condition,weather key, descption key
@@ -13,13 +17,13 @@ class Weather:
         self.city = city
         self.api_key = '5c940fbc1f7fa426e6c24c05fd945af8'
         self.data = None
-        self.get_condition()
-        self.get_current_temp()
-        self.advice()
-        self.get_feels()
-        self.get_humidity()
-        self.get_temp_max()
-        self.get_temp_min()
+        # self.get_condition()
+        # self.get_current_temp()
+        # self.advice()
+        # self.get_feels()
+        # self.get_humidity()
+        # self.get_temp_max()
+        # self.get_temp_min()
 
     #  key = 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
     # key 5c940fbc1f7fa426e6c24c05fd945af8
@@ -36,51 +40,51 @@ class Weather:
         # send a get request to url
         try:
             response = requests.get(url) #converting to json allows us to access individual attributes
+            response.raise_for_status()
             self.data =response.json()
             # for key,value in self.data.items():
             #     print(key,value)
             # print(self.data)
-            # return self.data
+            return True
         except requests.exceptions.RequestException as e:
             print(f'Error fetching data {e}')
-
+            return False
 
     def get_current_temp(self):  
+        if not self.data:
+            return None
         return self.data['main']['temp']
     
     def get_feels(self):
+        if not self.data:
+            return None
         return self.data['main']['feels_like']
     
     def get_temp_max(self):
-        '''
-        Docstring for get_temp
-        This function fetches the temperature of he entered city from the api
-        :param self: Description
-        '''
+        if not self.data:
+            return None
         return self.data['main']['temp_max']
 
     def get_temp_min(self):
+        if not self.data:
+            return None
         return self.data['main']['temp_min']
     
     def get_humidity(self):
+        if not self.data:
+            return None
         return self.data['main']['humidity']
 
     def get_condition(self):
-        '''
-        Docstring for get_condition
-        This function gets the city weather rcondition eg. rainy, cloudy, windy, showers etc.
-
-        :param self: Description
-        '''
+        if not self.data:
+            return None
         return self.data['weather'][0]['description']
 
     def advice(self):
         '''
-        Docstring for advice
         Based on the condition, this fuction returns basic advice to the user based on the weather condition
-
-        :param self: Description
         '''
+        condition = self.get_condition().lower()
 
         WEATHER_EMOJIS = {
                         "clear sky": "☀️",
@@ -96,13 +100,32 @@ class Weather:
                         "mist": "🌫️"
                     }
     
-        if 'sunny' or 'clear' in self.get_condition():
+        if 'sunny' or 'clear' in condition:
             ...
+        elif 'shower':
+
+        elif 'rain':
+
+        elif 'thunder':
+
+        elif 'snow':
+
+        elif 'fog' or 'mist' :
+
+        elif 'scattered clouds' or 'partially cloud':
     pass
 
+def main():
 
-city = input('City,Country:     ')
-# weather = Weather(city,api_key = '5c940fbc1f7fa426e6c24c05fd945af8')
-weather = Weather(city)
-weather.fetch_data()
-# print(weather.get_temp())
+    city = input('City:     ')
+    # weather = Weather(city,api_key = '5c940fbc1f7fa426e6c24c05fd945af8')
+    weather = Weather(city)
+    weather.fetch_data()
+    print(f'Condition:  {weather.get_condition()}')
+    print(f'Current temperature: {weather.get_current_temp():.0f}°C')
+    print(f'Feels like: {weather.get_feels():.0f}°C')
+    print(f'Max. temperature: {weather.get_temp_max():.0f}°C')
+    print(f'Min. temperature: {weather.get_temp_min():.0f}°C')
+
+if __name__=='__main__':
+    main()
